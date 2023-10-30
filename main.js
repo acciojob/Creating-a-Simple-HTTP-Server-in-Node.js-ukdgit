9a28ef5f608a6ab8c3d045dd0157a2db9aeb979b
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-10-30 13:21:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  // TODO: Send the ""Hello, world!"" response
+  res.write('Hello, world!');
+  res.end();
 });
 
 server.listen(port, () => {
-  // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server listening on port ${port}`);
 });
 
 // DO NOT EDIT BELOW THIS LINE
